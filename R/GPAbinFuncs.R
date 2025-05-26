4ccc21c3e8ab21a23e65733200dfaef52bd9890c
Johane NienkemperSwanepoel <nienkemperj@sun.ac.za>
2025-05-26 02:36:56 +0200

    initial file base

---
+++ R/GPAbinFuncs.R
@@ -0,0 +1,38 @@
+#' Deleting empty category levels
+#' 
+#' This function removes unobserved category levels before applying multivariate techniques.
+#'
+#' @param data is an object of the factor class
+#'
+#' @return
+#' {data} Returns the input object, now with empty category levels removed
+#' @export
+#'
+delCL <- function(data)
+{
+  ###################################################################################
+  ###################################################################################
+  #Information
+  #This function removes empty factor levels
+  ###################################################################################
+  #Arguments
+  #"data" is an object of the factor class
+  ###################################################################################
+  #Value
+  #"Returns the input object, now with empty category levels removed
+  ###################################################################################
+  ###################################################################################
+  for (j in 1:ncol(data))
+  {
+    col <- data[,j]
+    nl <- levels(col)
+    
+    for (i in 1:length(nl))
+    {
+      col <- droplevels(col,exclude=NA)
+    }
+    data[,j] <- col
+  }
+  return(data)
+}
+
